112c01fdc17e04a5ff12a4829c355d54f54027bc
AbarnaKumarasamy1122 <Abarnasamy1122@gmail.com>
2026-05-12 12:56:09 +0530

    fix issues

--- middleware.ts
+++ middleware.ts
@@ -15,8 +15,6 @@ export default handler;
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    '/((?!_next|api/inngest|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
   ],
 };
